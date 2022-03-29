b662565df1ee791714e4256ffeaf24f6c4935754
Sercan <sercanari1904@gmail.com>
2022-03-29 11:56:00 +0300

    ortalama hesapla app
    udemy derslerim

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:not_ortalama_hesaplama/constants/app_constants.dart';
+import 'package:not_ortalama_hesaplama/widgets/ortalama_hesapla_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dinamik Ortalama Hesapla',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Sabitler.anaRenk,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: OrtalamaHesaplaPage(),
+    );
+  }
+}
